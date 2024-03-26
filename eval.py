f72df8fbc6164aaca27fc03032d33c20c7efa567
harukaki <hrkkt1213@gmail.com>
2024-03-27 02:38:54 +0900

    add eval.py

---
+++ eval.py
@@ -0,0 +1,53 @@
+import jax
+from omegaconf import OmegaConf
+from pgx.bridge_bidding import BridgeBidding
+import pickle
+from src.evaluation import make_simple_duplicate_evaluate
+from pydantic import BaseModel
+
+
+class EVALConfig(BaseModel):
+    team1_model_path: str = None
+    team2_model_path: str = None
+    team1_activation: str = "relu"
+    team1_model_type: str = "DeepMind"
+    team2_activation: str = "relu"
+    team2_model_type: str = "DeepMind"
+    num_eval_envs: str = 100
+
+
+args = EVALConfig(**OmegaConf.to_object(OmegaConf.from_cli()))
+
+if __name__ == "__main__":
+    config = {
+        "team1_model_path": args.team1_model_path,
+        "team2_model_path": args.team2_model_path,
+        "team1_activation": args.team1_activation,
+        "team1_model_type": args.team1_model_type,
+        "team2_activation": args.team2_activation,
+        "team2_model_type": args.team2_model_type,
+        "num_eval_envs": args.num_eval_envs,
+    }
+    eval_env = BridgeBidding("dds_results/test_000.npy")
+    rng = jax.random.PRNGKey(0)
+    duplicate_evaluate = make_simple_duplicate_evaluate(
+        eval_env,
+        team1_activation=config["team1_activation"],
+        team1_model_type=config["team1_model_type"],
+        team2_activation=config["team2_activation"],
+        team2_model_type=config["team2_model_type"],
+        num_eval_envs=config["num_eval_envs"],
+    )
+    duplicate_evaluate = jax.jit(duplicate_evaluate)
+
+    print(f"num envs: {config['num_eval_envs']}")
+    team1_params = pickle.load(open(config["team1_model_path"], "rb"))
+    team2_params = pickle.load(open(config["team2_model_path"], "rb"))
+    print("---------------------------------------------------")
+    print(f'{config["team1_model_path"]} vs. {config["team1_model_path"]}')
+    log, tablea_info, tableb_info = duplicate_evaluate(
+        team1_params=team1_params,
+        team2_params=team2_params,
+        rng_key=rng,
+    )
+    print(f"IMP: {float(log[0])} ± {float(log[1])}")
